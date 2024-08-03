d8d4d47d7bd79ff10ba8ca492b232bfa33ac3304
Srikar Paida <36519233+thesrikarpaida@users.noreply.github.com>
2024-08-03 13:48:55 -0400

    Comments in level2.s

--- level2.s
+++ level2.s
@@ -4,9 +4,9 @@
 .section .text
 
 _start:
-    mov rdi, 0x2
-    mov rsi, 0x1
-    xor rdx, rdx
+    mov rdi, 0x2 # AF_INET - Internet IP Protocol
+    mov rsi, 0x1 # SOCK_STREAM - stream (connection) socket
+    xor rdx, rdx # NULL
     mov rax, 0x29 # 0x29 or 41 is the syscall value for socket()
     syscall
     mov rax, 0x3c # 0x3c or 60 is the syscall value for exit()
