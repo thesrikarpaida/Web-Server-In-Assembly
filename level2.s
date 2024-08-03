f9a6c601baae741f956174ee1a3f35bbaf590135
Srikar Paida <36519233+thesrikarpaida@users.noreply.github.com>
2024-08-03 13:51:19 -0400

    Update level2.s

--- level2.s
+++ level2.s
@@ -8,9 +8,10 @@ _start:
     mov rsi, 0x1 # SOCK_STREAM - stream (connection) socket
     xor rdx, rdx # NULL
     mov rax, 0x29 # 0x29 or 41 is the syscall value for socket()
-    syscall
+    syscall # socket(AF_INET, SOCK_STREAM, NULL)
+
+    xor rdi, rdi # 0
     mov rax, 0x3c # 0x3c or 60 is the syscall value for exit()
-    xor rdi, rdi
-    syscall
+    syscall # exit(0)
 
 .section .data
